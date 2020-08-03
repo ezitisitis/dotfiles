809a05daddaca66fe43632d8baeab7b3b59b747b
Marks Bogdanovs <marks@netservice.lv>
2020-08-03 10:16:36 +0300

    Improve docker functions

--- shell/.docker.sh
+++ shell/.docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start/stop nginx and stop/start dnsmasq
-function docker-proxy() {
+function docker-env() {
 
   ACTION=${1:=start}
 
@@ -18,4 +18,22 @@ function docker-proxy() {
   else
     echo 'Are you sure that you know what are you doing?'
   fi
+}
+
+function docker-ip() {
+  CONTAINER=${1:=error}
+
+  if [ $CONTAINER = 'error' ]
+  then
+    echo 'Please specify container name or id'
+  else
+    docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER
+  fi
+}
+
+function docker-auth() {
+  HTPASSWD_USERNAME=${1:=user}
+  HTPASSWD_PASSWORD=${2:=password}
+
+  echo $(htpasswd -nb $HTPASSWD_USERNAME $HTPASSWD_PASSWORD) | sed -e s/\\$/\\$\\$/g
 }
