1164562094611df48340f67de90fb08629114f30
Marks Bogdanovs <git@ezitisitis.com>
2020-09-20 21:28:23 +0300

    Replace .vimrc with init.vim in linking

--- install/links.sh
+++ install/links.sh
@@ -1,7 +1,6 @@
-echo "Link .vimrc"
-
-rm $HOME/.vimrc
-ln -s $HOME/.dotfiles/shell/configs/.vimrc $HOME/.vimrc
+echo "Link init.vim (neovim config)"
+rm $HOME/.config/nvim/init.vim
+ln -s $HOME/.dotfiles/shell/configs/init.vim $HOME/.config/nvim/init.vim
 
 echo "Link .zshrc"
 rm $HOME/.zshrc
