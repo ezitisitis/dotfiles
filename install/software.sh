30d8bd0c43b70b7a241264e711b992f29a4433f5
Marks Bogdanovs <git@ezitisitis.com>
2020-09-20 19:14:14 +0300

    Update software install

--- install/software.sh
+++ install/software.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-brew cask install iterm2
-brew cask install docker
-brew cask install virtualbox
-brew cask install postman
-brew cask install libreoffice
-brew cask install firefox-developer-edition
+brew ls iterm2 --versions --cask || brew cask install iterm2
+brew ls docker --versions --cask || brew cask install docker
+brew ls virtualbox --versions --cask || brew cask install virtualbox
+brew ls postman --versions --cask || brew cask install postman
+brew ls libreoffice --versions --cask || brew cask install libreoffice
+brew ls firefox-developer-edition --versions --cask || brew cask install firefox-developer-edition
+brew ls spotify --versions --cask || brew cask install spotify
