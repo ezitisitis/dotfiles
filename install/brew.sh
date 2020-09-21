507538547d32e45a7ab91b8adc162974e0901ac9
Marks Bogdanovs <git@ezitisitis.com>
2020-09-21 18:59:05 +0300

    Add shorthands and nvm in brew install script

--- install/brew.sh
+++ install/brew.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo "Install Homebrew (https://brew.sh/)"
+brew -v || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+echo "Install php 7.3"
 brew ls --versions php@7.3 || brew install php@7.3
+
+echo "Install php 7.4"
 brew ls --versions php@7.4 || brew install php@7.4
+
+echo "Install Neovim (https://neovim.io/)"
 brew ls --versions neovim || brew install neovim
+
+echo "Install MySQL 5.7"
 brew ls --versions mysql@5.7 || brew install mysql@5.7
+
+echo "Install composer (https://getcomposer.org/)"
+brew ls --versions composer || brew install composer
+
+echo "Install nvm"
+brew ls --versions nvm || brew install nvm
