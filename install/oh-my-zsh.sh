27209a7a47e9f727ae4b911fb4ed8986513945cf
Marks Bogdanovs <git@ezitisitis.com>
2020-10-10 17:21:44 +0300

    Add Zplug installation

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -3,5 +3,8 @@
 echo "Install Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Install Zplug"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
 echo "Install powerlevel9k theme"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
