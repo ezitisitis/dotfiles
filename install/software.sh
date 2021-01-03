35ec25247e27d4354193897afa6b7e17a863f1a3
Marks Bogdanovs <mail@ezitisitis.com>
2021-01-03 21:46:15 +0200

    Remove Firefox developer edition from software install cause it is not anymore in brew

--- install/software.sh
+++ install/software.sh
@@ -5,7 +5,6 @@ brew ls docker --versions --cask || brew install --cask docker
 brew ls virtualbox --versions --cask || brew install --cask virtualbox
 brew ls postman --versions --cask || brew install --cask postman
 brew ls libreoffice --versions --cask || brew install --cask libreoffice
-brew ls firefox-developer-edition --versions --cask || brew install --cask firefox-developer-edition
 brew ls spotify --versions --cask || brew install --cask spotify
 brew ls mactracker --versions --cask || brew install --cask mactracker
 brew ls discord --versions --cask || brew install --cask discord
