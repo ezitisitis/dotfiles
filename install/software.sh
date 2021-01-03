6f186a12ed2c05da670bb5cb4fe1a6a43a8a8ec6
Marks Bogdanovs <mail@ezitisitis.com>
2021-01-03 21:45:16 +0200

    Fix cask installation

--- install/software.sh
+++ install/software.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-brew ls iterm2 --versions --cask || brew cask install iterm2
-brew ls docker --versions --cask || brew cask install docker
-brew ls virtualbox --versions --cask || brew cask install virtualbox
-brew ls postman --versions --cask || brew cask install postman
-brew ls libreoffice --versions --cask || brew cask install libreoffice
-brew ls firefox-developer-edition --versions --cask || brew cask install firefox-developer-edition
-brew ls spotify --versions --cask || brew cask install spotify
-brew ls mactracker --versions --cask || brew cask install mactracker
-brew ls discord --versions --cask || brew cask install discord
+brew ls iterm2 --versions --cask || brew install --cask iterm2
+brew ls docker --versions --cask || brew install --cask docker
+brew ls virtualbox --versions --cask || brew install --cask virtualbox
+brew ls postman --versions --cask || brew install --cask postman
+brew ls libreoffice --versions --cask || brew install --cask libreoffice
+brew ls firefox-developer-edition --versions --cask || brew install --cask firefox-developer-edition
+brew ls spotify --versions --cask || brew install --cask spotify
+brew ls mactracker --versions --cask || brew install --cask mactracker
+brew ls discord --versions --cask || brew install --cask discord
