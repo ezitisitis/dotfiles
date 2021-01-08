38fcc57a05f068a48dadd51a0aaaa1ef369b7d24
Ezitis <git@ezitisitis.com>
2021-01-08 15:22:01 +0200

    Remove theme from oh-my-zsh

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -5,6 +5,3 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 echo "Install Zplug"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-echo "Install powerlevel9k theme"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
