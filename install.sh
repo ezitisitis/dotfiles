b5cee979a0b701f737e99e4bdbd107bf727c9734
Ezitis <git@ezitisitis.com>
2021-01-09 03:19:16 +0200

    Add install scripts to global install

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 echo "Starting global PC setup"
 source install/oh-my-zsh.sh
 source install/brew.sh
+source install/dnsmasq.sh
+source install/neovim.sh
+source install/links.sh
 source install/software.sh
