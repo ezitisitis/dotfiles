008839a14c6ba46454b6026c9815e06d4355e941
Marks Bogdanovs <git@ezitisitis.com>
2021-03-14 15:25:52 +0200

    Add install script for directories creation

---
+++ install/directories.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -d ~/web ]; then
+  mkdir -p ~/web
+fi
+
+if [ ! -d ~/devops ]; then
+  mkdir -p ~/devops
+fi
+
+if [ ! -d ~/tmp ]; then
+  mkdir -p ~/tmp
+fi
+
+if [ ! -d ~/tools ]; then
+  mkdir -p ~/tools
+fi
